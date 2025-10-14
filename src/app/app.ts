8d9f27b3169bd83051fb6071ca504028bd137023
Ana Laf <zanlamar@gmail.com>
2025-10-14 13:36:47 +0200

    basic routing structure

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,9 @@
 import { Component, signal } from '@angular/core';
-// import { RouterOutlet } from '@angular/router';
-import { Home } from "./src/app/home/home";
+import { RouterOutlet, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [Home],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
