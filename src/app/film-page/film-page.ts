c745147bd1b4003e49da6cd5ec7b02c247b0aa18
Ana Laf <zanlamar@gmail.com>
2025-10-16 10:31:59 +0200

    filmPage component created

---
+++ src/app/film-page/film-page.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+
+
+@Component({
+  selector: 'app-film-page',
+  imports: [],
+  templateUrl: './film-page.html',
+  styleUrl: './film-page.css'
+})
+export class FilmPage {
+
+}
