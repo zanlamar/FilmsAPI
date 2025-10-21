3bbc6ec6a652b501435082ff2f9fa656a29b3aa2
Ana Laf <zanlamar@gmail.com>
2025-10-21 13:31:08 +0200

    logic and register components just created

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,21 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, inject } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
+import { AuthService } from './shared/services/auth.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
+  
+  authService = inject(AuthService);
+  
+  onLogout() {
+    this.authService.logout();
+  }
+
   protected readonly title = signal('FilmsAPI');
 }
